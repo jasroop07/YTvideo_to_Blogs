10c15f2d150988ec8b58a4701df64d846aff4027
jasroop07 <jasroop07@gmail.com>
2025-07-21 23:44:01 +0530

    Specify Python version for Render

--- app.py
+++ app.py
@@ -59,6 +59,9 @@ def home():
 
 
 # --- Run the Application ---
+import os
+
 if __name__ == "__main__":
-    # debug=True allows you to see errors and automatically reloads the server on changes.
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT env variable
+    app.run(host="0.0.0.0", port=port, debug=True)
+
