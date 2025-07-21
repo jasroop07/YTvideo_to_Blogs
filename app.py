f432660a2b7c1eaee91f2734b6239a44eb4a0481
jasroop07 <jasroop07@gmail.com>
2025-07-22 00:46:41 +0530

    last

--- app.py
+++ app.py
@@ -62,6 +62,6 @@ def home():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render provides PORT env variable
+    port = int(os.environ.get("PORT", 8080))  # Render provides PORT env variable
     app.run(host="0.0.0.0", port=port, debug=True)
 
